16299cd5b2a0ecd7bd47426ae9d8349af23f3b25
themindstorm <parth.kabra@gmail.com>
2019-09-21 23:13:50 +0800

    removed publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const path = require("path");
 
 module.exports = {
   outputDir: path.resolve(__dirname, "docs"),
-  publicPath: '/website/'
+  // publicPath: '/website/'
   // assetsDir: "../../static/SPA"
 }
